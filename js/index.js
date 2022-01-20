8da801258fa6a9daefb501419dcca95c9139e82f
MikeTroianello <mike.troianello@gmail.com>
2022-01-20 14:21:21 -0800

    added steps 1-3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,57 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  const car = new Image();
+  car.src = "../images/car.png";
+  // car.onload = () => {
+  //   // ctx.drawImage(car, canvas.width / 2, canvas.height - 100, 50, 90);
+  // };
+
+  class Player {
+    constructor() {
+      this.x = canvas.width / 2;
+      this.y = canvas.height - 100;
+      this.image = car;
+    }
+
+    move(direction) {
+      switch (direction) {
+        case "ArrowLeft":
+          this.x -= 15;
+          break;
+        case "ArrowRight":
+          this.x += 15;
+          break;
+      }
+    }
+  }
+
+  const driver = new Player();
+
+  //Movements
+  document.addEventListener("keydown", function (e) {
+    switch (e.code) {
+      case "ArrowLeft":
+        driver.move("ArrowLeft");
+        break;
+      case "ArrowRight":
+        driver.move("ArrowRight");
+        break;
+    }
+  });
+
+  function startGame() {
+    animate();
+  }
+
+  function animate() {
+    window.requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(driver.image, driver.x, driver.y, 50, 90);
+  }
 };
